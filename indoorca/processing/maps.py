4444f6c7a2960c5c0c98fda86eb5a7dd5c648945
Felipe Felix Arias <felipefelixarias@gmail.com>
2023-04-06 14:01:21 -0500

    ORCA Wrapper and map processing

---
+++ indoorca/processing/maps.py
@@ -0,0 +1,134 @@
+import logging
+from typing import List, NamedTuple
+
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import measure
+from shapely import Polygon, MultiPolygon, Point
+
+
+class MapProcessor:
+    """ Class for processing a binary traversability map and generating obstcle
+    coordinates for the simulator.
+    """
+    def __init__(self, map: np.ndarray, pix_per_meter: int):
+        """ Initialize the MapProcessor.
+        
+        Parameters
+        ----------
+        map :
+            Binary traversability map
+        pix_per_meter :
+            Number of pixels per meter
+
+        """
+        self._map = self._crop_map(map)
+        self.pix_per_meter = pix_per_meter
+        self._obstacle_polygons = self._get_obstacle_polygons(map)
+
+
+    def _crop_map(self, tol: int=0) -> np.ndarray:
+        """ Return the cropped map.
+        
+        Parameters
+        ----------
+        tol :
+            Tolerance. If map is equal to tol, it will be cropped. Default is 0.
+        
+        Returns
+        -------
+        np.ndarray :
+            Cropped binary traversability map
+        """
+        # Find the min and max index of the non-zero elements
+        mask = self._map > tol
+        return self._map[np.ix_(mask.any(1), mask.any(0))]
+    
+    def _simplify_polygon(self, polygon: Polygon, tolerance: float=0.0) -> List[Polygon]:
+        """ Return the simplified polygon.
+        
+        Parameters
+        ----------
+        polygon :
+            Polygon to be simplified
+        tolerance :
+            Tolerance. Default is 0.0.
+        
+        Returns
+        -------
+        Polygon :
+            Simplified polygon
+        """
+        simplified = polygon.simplify(tolerance, preserve_topology=True)
+        if isinstance(simplified, Polygon):
+            return [simplified]
+        return list(simplified)
+    
+    def _get_obstacle_polygons(self, 
+                               tolerance: float=0.02, 
+                               min_area_ratio: float=0.0001 
+                               )-> List[Polygon]:
+        """Process the map and return a list of Polygon objects.
+
+        Parameters
+        ----------
+        tolerance : optional
+            Tolerance for simplifying the polygons, by default 0.02
+        min_area_ratio : optional
+            The threshold for ignoring obstacles, by default 0.0001
+
+        Returns
+        -------
+        List[Polygon]
+            List of Polygon objects extracted from the map
+        """        
+
+        contours = measure.find_contours(self._map, 0.5)
+
+        polygons = []
+        for contour in contours:
+            # Convert coordinates back to the original scale
+            scaled_contour = [((point[1] * 2) / (self._map.shape[1] - 1)) - 1 for point in contour]
+            scaled_contour_y = [((point[0] * 2) / (self._map.shape[0] - 1)) - 1 for point in contour]
+
+            # Ensure the contour has at least 3 points to form a valid polygon
+            if len(contour) < 3:
+                continue
+
+            # Create a Shapely polygon from the contour
+            polygon_coords = list(zip(scaled_contour, scaled_contour_y))
+            polygon = Polygon(polygon_coords)
+
+            # Discard very small polygons based on the min_area_ratio
+            if polygon.area < min_area_ratio:
+                continue
+
+            # Simplify the polygon and add it to the list
+            simplified_polygons = self._simplify_polygon(polygon, tolerance)
+            polygons.extend(simplified_polygons)
+
+        return polygons
+    
+    def get_obstacles(self, polygons: List[Polygon]) -> List[List[float]]:
+        """ Return a list of obstacles for the simulator.
+        
+        Parameters
+        ----------
+        polygons :
+            List of Polygon objects
+        
+        Returns
+        -------
+        List[List[float]] :
+            List of obstacles' vertices for the simulator
+        """
+        converted_polygons = []
+
+        for polygon in polygons:
+            # Convert coordinates to meters
+            meter_coords = [(coord[0] * self.pix_per_meter / 2, coord[1] * self.pix_per_meter / 2) for coord in polygon.exterior.coords]
+
+            # Append the list of tuples (coordinates) to the converted_polygons list
+            converted_polygons.append(meter_coords)
+
+        return converted_polygons
---
+++ indoorca/simulator/core.py
@@ -0,0 +1,211 @@
+import logging
+from typing import List, NamedTuple
+
+import numpy as np
+import rvo2
+
+
+class IndoorOrcaSimConfig(NamedTuple):
+    """ A wrapper for RVO2 simulator with additional functions for processing a binary traversability map and navigation. 
+    
+    Parameters
+    ----------
+    time_step :
+        The time step of the simulation. Must be positive
+    neighbor_dist :
+        The default maximum distance (center point to center point) to other agents a agent takes into account 
+        in the navigation. The larger this number, the longer the running time of the simulation. If the number 
+        is too low, the simulation will not be safe. Must be non-negative
+    max_neighbors :
+        The default maximum number of other agents a agent takes into account in the navigation. The larger 
+        this number, the longer the running time of the simulation. If the number is too low, the simulation 
+        will not be safe. Must be non-negative
+    time_horizon :
+        The default minimum amount of time for which a agent's velocities that are computed by the simulation 
+        are safe with respect to other agents. The larger this number, the sooner a agent will respond to the 
+        presence of other agents, but the less freedom a agent has in choosing its velocities. Must be positive
+    time_horizon_obst :
+        The default minimum amount of time for which a agent's velocities that are computed by the simulation 
+        are safe with respect to obstacles. The larger this number, the sooner a agent will respond to the 
+        presence of obstacles, but the less freedom a agent has in choosing its velocities. Must be positive
+    radius :
+        The default radius of a agent. Must be non-negative
+    max_speed :
+        The default maximum speed of a agent. Must be non-negative
+    velocity :
+        The default initial two-dimensional linear velocity of a agent (optional)
+    """
+    
+    time_step: float = 1/60.
+    neighbor_dist: float = 1.5
+    max_neighbors: int = 5
+    time_horizon: float = 1.5
+    time_horizon_obst: float = 2
+    radius: float = 0.4
+    max_speed: float = 2
+    velocity: List[float] = None
+
+
+class IndoorORCASim:
+    """ Wrapper for RVO2 simulator with additional functions for processing a binary traversability map and navigation.
+    
+    Parameters
+    ----------
+    config :
+        Configuration for the simulator
+
+    Attributes
+    ----------
+    sim :
+        RVO2 simulator
+    """
+    def __init__(self, config: IndoorOrcaSimConfig):
+        self.config = config
+        self.sim = rvo2.PyRVOSimulator(config.time_step, config.neighbor_dist, config.max_neighbors, config.time_horizon, 
+                                       config.time_horizon_obst, config.radius, config.max_speed, config.velocity)
+    
+    def add_agent(self, position: List[float], velocity: List[float] = None) -> int:
+        """ Adds an agent to the simulation.
+        
+        Parameters
+        ----------
+        position :
+            The two-dimensional starting position of this agent
+        velocity : 
+            The two-dimensional starting velocity of this agent (optional)
+
+        Returns
+        -------
+        int : 
+            The number of the agent
+        """
+        return self.sim.addAgent(position, self.config.neighbor_dist, self.config.max_neighbors, self.config.time_horizon, 
+                                 self.config.time_horizon_obst, self.config.radius, self.config.max_speed, velocity)
+    
+    def add_obstacle(self, vertices: List[List[float]]) -> int:
+        """ Adds an obstacle to the simulation.
+        
+        Parameters
+        ----------
+        vertices : List[List[float]]
+            A list of vertices of the polygonal obstacle in counterclockwise order
+
+        Returns
+        -------
+        int :
+            The number of the obstacle
+        """
+        return self.sim.addObstacle(vertices)
+    
+    def process_obstacles(self) -> None:
+        """ Processes the obstacles that have been added so that they are accounted for in the simulation.
+        """
+        self.sim.processObstacles()
+
+    def set_agent_pref_velocity(self, agent_no: int, pref_velocity: List[float]) -> None:
+        """ Sets the preferred velocity of a agent.
+        
+        Parameters
+        ----------
+        agent_no :
+            The number of the agent whose preferred velocity is to be set
+        pref_velocity :
+            The two-dimensional preferred velocity of the agent
+        """
+        self.sim.setAgentPrefVelocity(agent_no, pref_velocity)
+
+    def set_agent_position(self, agent_no: int, position: List[float]) -> None:
+        """ Sets the position of a agent.
+        
+        Parameters
+        ----------
+        agent_no :
+            The number of the agent whose position is to be set
+        position :
+            The two-dimensional position of the agent
+        """
+        self.sim.setAgentPosition(agent_no, position)
+
+    def get_num_agents(self) -> int:
+        """ Returns the number of agents in the simulation.
+        
+        Returns
+        -------
+        int :
+            The number of agents in the simulation
+        """
+        return self.sim.getNumAgents()
+    
+    def get_num_obstacles(self) -> int:
+        """ Returns the number of obstacles in the simulation.
+        
+        Returns
+        -------
+        int :
+            The number of obstacles in the simulation
+        """
+        return self.sim.getNumObstacles()
+    
+    def get_num_obstacle_vertices(self) -> int:
+        """ Returns the number of obstacle vertices.
+
+        Returns
+        -------
+        int :
+            The number of vertices of the obstacle
+        """
+        return self.sim.getNumObstacleVertices()
+    
+    def get_agent_position(self, agent_no: int) -> List[float]:
+        """ Returns the position of a agent.
+        
+        Parameters
+        ----------
+        agent_no :
+            The number of the agent whose position is to be retrieved
+
+        Returns
+        -------
+        List[float] :
+            The two-dimensional position of the agent
+        """
+        return self.sim.getAgentPosition(agent_no)
+    
+    def get_agent_velocity(self, agent_no: int) -> List[float]:
+        """ Returns the velocity of a agent.
+        
+        Parameters
+        ----------
+        agent_no :
+            The number of the agent whose velocity is to be retrieved
+
+        Returns
+        -------
+        List[float] :
+            The two-dimensional velocity of the agent
+        """
+        return self.sim.getAgentVelocity(agent_no)
+    
+    def get_global_time(self) -> float:
+        """ Returns the global time of the simulation.
+        
+        Returns
+        -------
+        float :
+            The global time of the simulation
+        """
+        return self.sim.getGlobalTime()
+    
+    def do_step(self) -> None:
+        """ Performs a simulation step and updates the two-dimensional position and two-dimensional velocity of each agent.
+        """
+        self.sim.doStep()
+    
+
+    
+
+
+
+
+    
+
